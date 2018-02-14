607d3f5126f309fc76572d8b8dd7aac16df206bb
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2018-02-14 11:15:09 -0800

    Check for existence of imageFiles/tmp_tomcat/tomcat.jks

--- generateTLSCert.sh
+++ generateTLSCert.sh
@@ -4,15 +4,20 @@ if [ -z "$KEYSIZE" ]; then
   KEYSIZE=4096
 fi
 
-echo "Generating key.  This can take a few seconds."
-keytool -genkey -alias tomcat -keyalg RSA -keysize $KEYSIZE \
-       -validity 10000 \
-       -storepass "changeit" -keypass "changeit" \
-       -dname "CN=bidms-tomcat,OU=BIDMS Tomcat Docker Dev" \
-       -keystore imageFiles/tmp_tomcat/tomcat.jks
-chmod 600 imageFiles/tmp_tomcat/tomcat.jks
-
-keytool -exportcert -alias tomcat \
-  -storepass "changeit"  -keystore imageFiles/tmp_tomcat/tomcat.jks \
-  -rfc \
-  -file imageFiles/tmp_tomcat/tomcat_pubkey.pem
+if [ !-e imageFiles/tmp_tomcat/tomcat.jks ]; then
+  echo "Generating key.  This can take a few seconds."
+  keytool -genkey -alias tomcat -keyalg RSA -keysize $KEYSIZE \
+         -validity 10000 \
+         -storepass "changeit" -keypass "changeit" \
+         -dname "CN=bidms-tomcat,OU=BIDMS Tomcat Docker Dev" \
+         -keystore imageFiles/tmp_tomcat/tomcat.jks
+  chmod 600 imageFiles/tmp_tomcat/tomcat.jks
+  
+  keytool -exportcert -alias tomcat \
+    -storepass "changeit"  -keystore imageFiles/tmp_tomcat/tomcat.jks \
+    -rfc \
+    -file imageFiles/tmp_tomcat/tomcat_pubkey.pem
+else
+  echo "imageFiles/tmp_tomcat/tomcat.jks already exists"
+  exit 1
+fi
