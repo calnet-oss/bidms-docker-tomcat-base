1a5061312c8ff4187af488acf1f8ca0877d3d523
Brian Koehmstedt <bkoehmstedt@berkeley.edu>
2021-02-19 07:55:25 -0800

    Revert "Quote the build-args"
    This reverts commit 27b78f8b1070dfd3db5a27aeebad5a4561690c71.

--- buildImage.sh
+++ buildImage.sh
@@ -73,22 +73,22 @@ if [ ! -z "$NETWORK" ]; then
 fi
 
 if [ ! -z "$APT_PROXY_URL" ]; then
-  ARGS+="--build-arg APT_PROXY_URL=\"$APT_PROXY_URL\" "
+  ARGS+="--build-arg APT_PROXY_URL=$APT_PROXY_URL "
 elif [ -e $HOME/.aptproxy ]; then
   apt_proxy_url=$(cat $HOME/.aptproxy)
-  ARGS+="--build-arg APT_PROXY_URL=\"$apt_proxy_url\" "
+  ARGS+="--build-arg APT_PROXY_URL=$apt_proxy_url "
 fi
 
 if [ ! -z "JAVA_MEM_OPTS" ]; then
-  ARGS+="--build-arg JAVA_MEM_OPTS=\"$JAVA_MEM_OPTS\" "
+  ARGS+="--build-arg JAVA_MEM_OPTS=$JAVA_MEM_OPTS "
 fi
 
 if [ ! -z "JAVA_DEBUG_OPTS" ]; then
-  ARGS+="--build-arg JAVA_DEBUG_OPTS=\"$JAVA_DEBUG_OPTS\" "
+  ARGS+="--build-arg JAVA_DEBUG_OPTS=$JAVA_DEBUG_OPTS "
 fi
 
 if [ ! -z "JNDI_DB_URL" ]; then
-  ARGS+="--build-arg JNDI_DB_URL=\"$JNDI_DB_URL\" "
+  ARGS+="--build-arg JNDI_DB_URL=$JNDI_DB_URL "
 fi
 
 if [ ! -z "$(echo \"$BUILDTIME_CMD\" | grep buildah)" ]; then
